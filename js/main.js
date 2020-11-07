83dff0f7b67b76155e595fe66e670e24c3184e53
KevinSuasnabar <ksuasnabar@causayrumbo.pe>
2020-11-07 12:43:15 -0500

    disenio no responsive

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function imgSlider(any) {
+    document.querySelector('.pepsi').src = any;
+}
+
+function changeBgColor(color) {
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
